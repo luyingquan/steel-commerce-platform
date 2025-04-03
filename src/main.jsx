270020d58e667453e369b0993e4cf29392d52632
10060755 <996760697@qq.com>
2025-04-03 19:22:23 +0800

    修复GitHub Pages SPA路由问题

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/steel-commerce-platform">
       <App />
     </BrowserRouter>
   </StrictMode>,
